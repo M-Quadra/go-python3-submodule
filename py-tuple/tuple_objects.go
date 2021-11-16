c7d93ee8ebf35291b0b8e8cfd75bb51fb6285669
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2021-11-16 23:10:19 +0800

    add PyTuple

--- py-tuple/tuple_objects.go
+++ py-tuple/tuple_objects.go
@@ -39,6 +39,10 @@ func New(len int) *python.PyObject {
 
 // Size PyTuple_Size
 func Size(p *python.PyObject) int {
+	if p == nil {
+		return 0
+	}
+
 	return int(C.PyTuple_Size(toC(p)))
 }
 
@@ -46,6 +50,10 @@ func Size(p *python.PyObject) int {
 
 // GetItem PyTuple_GetItem
 func GetItem(p *python.PyObject, pos int) *python.PyObject {
+	if p == nil {
+		return nil
+	}
+
 	return toObject(C.PyTuple_GetItem(toC(p), (C.Py_ssize_t)(pos)))
 }
 
@@ -58,6 +66,10 @@ func GetSlice(p *python.PyObject, low, high int) *python.PyObject {
 
 // SetItem PyTuple_SetItem
 func SetItem(p *python.PyObject, pos int, o *python.PyObject) bool {
+	if p == nil {
+		return false
+	}
+
 	return C.PyTuple_SetItem(toC(p), C.Py_ssize_t(pos), toC(o)) == 0
 }
 
---
+++ test/py_tuple_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/M-Quadra/go-python3-submodule/py"
+	pylist "github.com/M-Quadra/go-python3-submodule/py-list"
+	pylong "github.com/M-Quadra/go-python3-submodule/py-long"
+	pytuple "github.com/M-Quadra/go-python3-submodule/py-tuple"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPyTupleCheck(t *testing.T) {
+	pytuple.Check(nil)
+	pytuple.CheckExact(nil)
+
+	tuple := pytuple.New(0)
+	defer py.DecRef(tuple)
+	assert.True(t, pytuple.Check(tuple))
+	assert.True(t, pytuple.CheckExact(tuple))
+
+	list := pylist.New(0)
+	defer py.DecRef(list)
+	assert.False(t, pytuple.Check(list))
+	assert.False(t, pytuple.CheckExact(list))
+}
+
+func TestPyTupleNew(t *testing.T) {
+	tuple := pytuple.New(0)
+	defer py.DecRef(tuple)
+	assert.NotNil(t, tuple)
+}
+
+func TestPyTupleSize(t *testing.T) {
+	assert.Zero(t, pytuple.Size(nil))
+
+	l := rand.Intn(1000)
+	tuple := pytuple.New(l)
+	defer py.DecRef(tuple)
+
+	assert.Equal(t, l, pytuple.Size(tuple))
+}
+
+func TestPyTupleGetItem(t *testing.T) {
+	assert.Nil(t, pytuple.GetItem(nil, 0))
+
+	tuple := pytuple.New(1)
+	defer py.DecRef(tuple)
+	assert.Nil(t, pytuple.GetItem(tuple, -1))
+
+	v := rand.Intn(1000)
+	long := pylong.FromInt(v)
+	defer py.DecRef(long)
+
+	assert.True(t, pytuple.SetItem(tuple, 0, long))
+	assert.Equal(t, v, pylong.AsInt(pytuple.GetItem(tuple, 0)))
+}
+
+func TestPyTupleGetSlice(t *testing.T) {
+	assert.Nil(t, pytuple.GetSlice(nil, 0, 0))
+
+	tupleA := pytuple.New(1)
+	defer py.DecRef(tupleA)
+
+	tupleB := pytuple.GetSlice(tupleA, 0, -2)
+	defer py.DecRef(tupleB)
+	assert.Zero(t, pytuple.Size(tupleB))
+
+	tupleC := pytuple.GetSlice(tupleA, 0, 2)
+	defer py.DecRef(tupleC)
+	assert.Equal(t, 1, pytuple.Size(tupleC))
+}
+
+func TestPyTupleSetItem(t *testing.T) {
+	v := rand.Intn(1000)
+	long := pylong.FromInt(v)
+	defer py.DecRef(long)
+
+	assert.False(t, pytuple.SetItem(nil, 0, long))
+
+	tuple := pytuple.New(1)
+	defer py.DecRef(tuple)
+
+	assert.True(t, pytuple.SetItem(tuple, 0, nil))
+	assert.False(t, pytuple.SetItem(tuple, -1, long))
+	assert.True(t, pytuple.SetItem(tuple, 0, long))
+
+	assert.Equal(t, v, pylong.AsInt(pytuple.GetItem(tuple, 0)))
+}
