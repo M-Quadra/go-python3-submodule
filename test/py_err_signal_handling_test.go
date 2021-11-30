3f436767bf6be6d86408f8a76c318cb0da822a0d
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2021-11-30 22:54:16 +0800

    fix TestPyErrCheckSignals

--- test/py_err_signal_handling_test.go
+++ test/py_err_signal_handling_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	pyerr "github.com/M-Quadra/go-python3-submodule/py-err"
@@ -12,12 +13,21 @@ import (
 func TestPyErrCheckSignals(t *testing.T) {
 	fmt.Println(assert.CallerInfo()[0])
 
-	pyerr.Clear()
-
 	pyerr.SetInterrupt()
-	assert.Equal(t, -1, pyerr.CheckSignals())
 
-	assert.True(t, pyerr.GivenExceptionMatches(pyerr.Occurred(), pyexc.TypeError))
-	pyerr.Print()
-	assert.Nil(t, pyerr.Occurred())
+	switch runtime.GOOS {
+	case "linux":
+		assert.Nil(t, pyerr.Occurred())
+		assert.Equal(t, 0, pyerr.CheckSignals())
+		assert.Nil(t, pyerr.Occurred())
+	default:
+		assert.Nil(t, pyerr.Occurred())
+		assert.Equal(t, -1, pyerr.CheckSignals())
+		assert.NotNil(t, pyerr.Occurred())
+
+		// TypeError: 'NoneType' object is not callable
+		assert.True(t, pyerr.GivenExceptionMatches(pyerr.Occurred(), pyexc.TypeError))
+		pyerr.Print()
+		assert.Nil(t, pyerr.Occurred())
+	}
 }
